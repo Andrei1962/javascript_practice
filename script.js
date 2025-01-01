fffc5c520214fe1c6049efb3b3ebd12974c2bc70
Andrei Stolarov <andrey.stolyarov.1962@mail.ru>
2025-01-01 20:36:30 +0300

    создали переменные2

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let oil= 'synthetics'
 let many= 1000000000000000n
 let included= true
 let remains= null
-let example= undefined
-console.log('Переменная floor хранит внутри значение:', floor, 'и относится к типу данных', typeof floor);
-console.log('Переменная oil хранит внутри значение:', oil, ' иотносится к типу данных', typeof oil);
-console.log('Переменная many хранит внутри значение:', many,' иотносится к типу данных', typeof many);
-console.log('Переменная included хранит внутри значение:',included, 'и относится к типу данных', typeof included);
-console.log('Переменная remains хранит внутри значение:', remains, 'и относится к типу данных ', typeof remains);
-console.log('Переменная example хранит внутри значение:', example, 'и относится к типу данных', typeof example);
+let example= 
+console.log(`Переменная floor хранит внутри значение: ${floor} и относится к типу данных ${typeof floor}`);
+console.log(`Переменная oil хранит внутри значение: ${oil} и относится к типу данных ${typeof oil}`);
+console.log(`Переменная many хранит внутри значение: ${many} и относится к типу данных ${typeof many}`);
+console.log(`Переменная included хранит внутри значение: ${included} и относится к типу данных ${typeof included}`);
+console.log(`Переменная remains хранит внутри значение: ${remains} и относится к типу данных ${typeof remains}`);
+console.log(`Переменная example хранит внутри значение: ${example} и относится к типу данных ${typeof example}`);
