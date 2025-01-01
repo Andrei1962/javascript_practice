3aa33df77e651a27c02fb8038b94e3b3f964cc51
Andrei Stolarov <andrey.stolyarov.1962@mail.ru>
2025-01-01 20:44:37 +0300

    создали калкулятор2

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 let oneNamber = +prompt('Введите первое число');
 let operator = prompt('Введите действие + , - , * , / ');
 let twoNamber = +prompt('Введите второе число');
-let result;
-if (operator === '+') {
- result = oneNamber + twoNamber
-} else if (operator === '-') {
-  result = oneNamber - twoNamber
-}else if (operator === '*') {
-  result = oneNamber * twoNamber
-}else if (operator === '/') {
-  result = oneNamber / twoNamber
-}
-alert(result);
-
-/*alert('Результат вычисления: ${oneNamber} ${operator} ${twoNamber} = ${eval('${oneNamber} ${operator} ${twoNamber}')}')
- 
-/*alert('Результат ${oneNamber} ${operator} ${twoNamber} = ${eval('${oneNamber} ${operator} ${twoNamber}')}');
-alert(oneNamber);/* так выводит */
-/*alert('результат ${oneNamber}') /* так не выводит */
+alert(`Результат вычисления: ${oneNamber} ${operator} ${twoNamber} = ${eval(`${oneNamber} ${operator} ${twoNamber}`)}`)
