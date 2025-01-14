e902d83444d25cc34448e5537ba5aff1ce58533b
Andrei Stolarov <andrey.stolyarov.1962@mail.ru>
2025-01-14 11:20:26 +0300

    числа и строки

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let text = '  я СтуденТ КуРса  FronteNd РоЗРобОТКЭ    ';
-let replaceText = text.replace("РоЗРобОТКЭ", "разработчик");        /*заменяем слово*/
-let trimmedText = replaceText.trim();                               /*удаляем пробелы вначале и в конце*/
-let LowerCasetext = trimmedText.toLowerCase() ;                     /*переводим в нижний регистр*/                
-Result = LowerCasetext.replace("я", "Я Андрей,");                   /*делаем заглавную букву и добавляем имя*/
-console.log(Result);
+let result = text.trim().replace("РоЗРобОТКЭ", "разработчик").toLowerCase().replace("я", "Я Андрей,").replace("курса ", "курса");
+console.log(result);
+/*.replace("РоЗРобОТКЭ", "разработчик");        /*заменяем слово*/
+/*.Text.trim();                                 /*удаляем пробелы вначале и в конце*/
+/*.toLowerCase() ;                              /*переводим в нижний регистр*/                
+/*.replace("я", "Я Андрей,");                   /*делаем заглавную букву и добавляем имя*/
+/* replace("курса ", "курса");                  /*удаляем лишний пробел*/            
